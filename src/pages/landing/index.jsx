2ac54be243eb1d5321c54cad81f6bc65803fc1a4
Ellen Ntiamoah <ellenntiamoah@gmail.com>
2025-06-02 10:45:12 +0000

    Service Updates

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Footer from "../../components/Footer";
 import Services from "./components/services";
 
+
 const LandingPage = () => {
   return (
     <div>
