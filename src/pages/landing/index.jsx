0aba81b2058b5cc60e17511b8fc3e0c6057d2ccb
margaret boye <margaretboye8@gmail.com>
2025-06-02 11:47:44 +0000

    commit

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import About from "./components/About";
 import Footer from "../../components/footer";
-import Hero from "./components/Hero";
+import Hero from "./components/hero";
 
 const LandingPage = () => {
   return (
