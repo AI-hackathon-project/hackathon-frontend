c9d5dc08a1b1d1b8acb735ecf4953df4fb9b1c2e
margaret boye <margaretboye8@gmail.com>
2025-06-02 11:37:41 +0000

    footer path

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import Hero from "./components/hero";
+
 import About from "./components/About";
-import Footer from "../../components/Footer";
+import Hero from "./components/Hero";
+import Footer from "../../components/footer";
 
 const LandingPage = () => {
   return (
