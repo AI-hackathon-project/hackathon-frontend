15121b0f759bef166f81bc58d30a575b954fc697
Sarah0360 <amediorsarah@gmail.com>
2025-06-04 19:23:40 +0000

    updates

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -2,8 +2,8 @@ import Hero from "./components/hero";
 import About from "./components/about";
 import Footer from "../../components/footer";
 import Navbar from "../../components/navbar";
-import Contacts from "./components/Contact";
-import Testimonials from "./components/Testimonials";
+import Contacts from "./components/contact";
+import Testimonials from "./components/testimonials";
 const LandingPage = () => {
   return (
     <div>
